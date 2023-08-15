042831bb4fd08af008593a6b44bf0e2128d8552a
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-15 19:26:57 -0300

    toda vez que resolver uma questõa deste site vou guarda-la aqui

---
+++ beginner/1003-SimpleSum.js
@@ -0,0 +1,9 @@
+/*
+    Read two integer values, in this case, the variables A and B. After this, calculate the sum between them and assign it to the variable SOMA. Write the value of this variable.
+*/
+
+let A = Number(lines[0]);
+let B = Number(lines[1]);
+
+let SOMA = A + B;
+console.log(`SOMA = ${SOMA}`);
