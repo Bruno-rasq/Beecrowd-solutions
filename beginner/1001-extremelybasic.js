76bc42db8e7a0ae9ac84e4f7435f939323d615c4
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-15 19:17:35 -0300

    iniciando algumas questões

---
+++ beginner/1001-extremelybasic.js
@@ -0,0 +1,10 @@
+/*
+    Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.
+
+*/
+
+let A = Number(lines[0]);
+let B = Number(lines[1]);
+let X = A + B;
+
+console.log(`X = ${X}`);
