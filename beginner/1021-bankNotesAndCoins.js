26f08ca36fabc97f8ec770e0c14c1e567b3ae36f
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-21 16:17:22 -0300

    aparentemente o codigo estava lendo o input como string, usei o metodo map para alterar isso

--- beginner/1021-bankNotesAndCoins.js
+++ beginner/1021-bankNotesAndCoins.js
@@ -1,58 +1,55 @@
-function bankNotesWithCoins(n) {
 
-    // notes
-    let cen = Math.floor(n / 100);
-    let rest100 = n % 100;
-
-    let cin = Math.floor(rest100 / 50);
-    let rest50 = rest100 % 50;
-
-    let vin = Math.floor(rest50 / 20);
-    let rest20 = rest50 % 20;
-
-    let dez = Math.floor(rest20 / 10);
-    let rest10 = rest20 % 10;
-
-    let cinco = Math.floor(rest10 / 5);
-    let rest5 = rest10 % 5;
-
-    let dois = Math.floor(rest5 / 2);
-    let rest2 = rest5 % 2;
-
-    let um = Math.floor(rest2 / 1);
-    let rest1 = rest2 % 1
-
-    // coins
-    let cent50 = Math.floor(rest1 / 0.50);
-
-    let cent25 = Math.floor((rest1 - (0.50 * cent50)) / 0.25);
-
-    let cent10 = Math.floor((rest1 - (0.50 * cent50) - (0.25 * cent25)) / 0.10);
-
-    let cent5 = Math.floor((rest1 - (0.50 * cent50) - (0.25 * cent25) - (0.10 * cent10)) / 0.05);
-
-    let cent1 = Math.floor((rest1 - (0.50 * cent50) - (0.25 * cent25) - (0.10 * cent10) - (0.05 * cent5)) / 0.01)
+let n = input.split(" ").map(item => parseFloat(item));
 
 
-    console.log("NOTAS:");
-    console.log(`${cen.toFixed(0)} nota(s) de R$ 100.00`);
-    console.log(`${cin.toFixed(0)} nota(s) de R$ 50.00`);
-    console.log(`${vin.toFixed(0)} nota(s) de R$ 20.00`);
-    console.log(`${dez.toFixed(0)} nota(s) de R$ 10.00`);
-    console.log(`${cinco.toFixed(0)} nota(s) de R$ 5.00`);
-    console.log(`${dois.toFixed(0)} nota(s) de R$ 2.00`);
+let CEM = Math.floor(n / 100);
 
-    console.log("MOEDAS:");
-    console.log(`${um.toFixed(0)} moeda(s) de R$ 1.00`);
-    console.log(`${cent50.toFixed(0)} moeda(s) de R$ 0.50`);
-    console.log(`${cent25.toFixed(0)} moeda(s) de R$ 0.25`);
-    console.log(`${cent10.toFixed(0)} moeda(s) de R$ 0.10`);
-    console.log(`${cent5.toFixed(0)} moeda(s) de R$ 0.05`);
-    console.log(`${cent1.toFixed(0)} moeda(s) de R$ 0.01`);
+let CINQUENTA = Math.floor((n - (100 * CEM)) / 50);
 
-}
+let VINTE = Math.floor((n - (100 * CEM) - (50 * CINQUENTA)) / 20);
 
-bankNotesWithCoins(576.73);
-bankNotesWithCoins(4);
-bankNotesWithCoins(91.09);
+let DEZ = Math.floor((n - (100 * CEM) - (50 * CINQUENTA) - (20 * VINTE)) / 10);
+
+let CINCO = Math.floor((n - (100 * CEM) - (50 * CINQUENTA) - (20 * VINTE) - (10 * DEZ)) / 5);
+
+let DOIS = Math.floor((n - (100 * CEM) - (50 * CINQUENTA) - (20 * VINTE) - (10 * DEZ) - (5 * CINCO)) / 2);
+
+
+let restCEM = n % 100;
+let restCINQ = restCEM % 50;
+let restVINT = restCINQ % 20;
+let restDEZ = restVINT % 10;
+let restCINC = restDEZ % 5;
+let restDOIS = restCINC % 2;
+
+
+let Coins = restDOIS * 100;
+
+let Coin1 = Math.floor(Coins / 100);
+
+let cent50 = Math.floor((Coins - (100 * Coin1)) / 50);
+
+let cent25 = Math.floor((Coins - (100 * Coin1) - (50 * cent50)) / 25);
+
+let cent10 = Math.floor((Coins - (100 * Coin1) - (50 * cent50) - (25 * cent25)) / 10);
+
+let cent5 = Math.floor((Coins - (100 * Coin1) - (50 * cent50) - (25 * cent25) - (10 * cent10)) / 5);
+
+let cent1 = Math.floor((Coins - (100 * Coin1) - (50 * cent50) - (25 * cent25) - (10 * cent10) - (5 * cent5)) / 1);
+
+console.log("NOTAS:");
+console.log(`${CEM.toFixed(0)} nota(s) de R$ 100.00`);
+console.log(`${CINQUENTA.toFixed(0)} nota(s) de R$ 50.00`);
+console.log(`${VINTE.toFixed(0)} nota(s) de R$ 20.00`);
+console.log(`${DEZ.toFixed(0)} nota(s) de R$ 10.00`);
+console.log(`${CINCO.toFixed(0)} nota(s) de R$ 5.00`);
+console.log(`${DOIS.toFixed(0)} nota(s) de R$ 2.00`);
+
+console.log("MOEDAS:");
+console.log(`${Coin1.toFixed(0)} moeda(s) de R$ 1.00`);
+console.log(`${cent50.toFixed(0)} moeda(s) de R$ 0.50`);
+console.log(`${cent25.toFixed(0)} moeda(s) de R$ 0.25`);
+console.log(`${cent10.toFixed(0)} moeda(s) de R$ 0.10`);
+console.log(`${cent5.toFixed(0)} moeda(s) de R$ 0.05`);
+console.log(`${cent1.toFixed(0)} moeda(s) de R$ 0.01`);
 
--- test/test.js
+++ test/test.js
@@ -1,17 +1,57 @@
-let A = 3.0;
-let B = 4.0;
-let C = 5.2;
+function bankNotesWithCoins(n) {
 
-let PI = 3.14159;
+   
+    let CEM = Math.floor( n / 100 );
 
-let AreaTring = (A * C) / 2.0;
-let circle = PI * Math.pow(C, 2);
-let trapezium = ((A + B) * C) / 2.0;
-let square = Math.pow(B, 2);
-let retangle = A * B;
+    let CINQUENTA = Math.floor(( n - (100 * CEM) ) / 50);
 
-console.log(`TRIANGULO: ${AreaTring.toFixed(3)}`);
-console.log(`CIRCULO: ${circle.toFixed(3)}`);
-console.log(`TRAPEZIO: ${trapezium.toFixed(3)}`);
-console.log(`QUADRADO: ${square.toFixed(3)}`);
-console.log(`RETANGULO: ${retangle.toFixed(3)}`);
+    let VINTE = Math.floor((n - (100 * CEM) - (50 * CINQUENTA)) / 20); 
+
+    let DEZ = Math.floor((n - (100 * CEM) - ( 50 * CINQUENTA) - (20 * VINTE)) / 10);
+
+    let CINCO = Math.floor((n - (100 * CEM) - ( 50 * CINQUENTA) - (20 * VINTE) - (10 * DEZ)) / 5);
+
+    let DOIS = Math.floor((n - (100 * CEM) - ( 50 * CINQUENTA) - (20 * VINTE) - (10 * DEZ) - (5 * CINCO)) / 2);
+
+
+    let restCEM = n % 100;
+    let restCINQ = restCEM % 50;
+    let restVINT = restCINQ % 20;
+    let restDEZ = restVINT % 10;
+    let restCINC = restDEZ % 5;
+    let restDOIS = restCINC % 2;
+
+
+    let Coins = restDOIS * 100;
+
+    let Coin1 = Math.floor( Coins / 100);
+
+    let cent50 = Math.floor( (Coins - (100 * Coin1)) / 50);
+
+    let cent25 = Math.floor( (Coins - (100 * Coin1) - (50 * cent50)) / 25);
+
+    let cent10 = Math.floor( (Coins - (100 * Coin1) - (50 * cent50) - (25 * cent25)) / 10);
+
+    let cent5 = Math.floor( (Coins - (100 * Coin1) - (50 * cent50) - (25 * cent25) - (10 * cent10)) / 5);
+
+    let cent1 = Math.floor( (Coins - (100 * Coin1) - (50 * cent50) - (25 * cent25) - (10 * cent10) - (5 * cent5)) / 1);
+
+    console.log("NOTAS:");
+    console.log(`${CEM.toFixed(0)} nota(s) de R$ 100.00`);
+    console.log(`${CINQUENTA.toFixed(0)} nota(s) de R$ 50.00`);
+    console.log(`${VINTE.toFixed(0)} nota(s) de R$ 20.00`);
+    console.log(`${DEZ.toFixed(0)} nota(s) de R$ 10.00`);
+    console.log(`${CINCO.toFixed(0)} nota(s) de R$ 5.00`);
+    console.log(`${DOIS.toFixed(0)} nota(s) de R$ 2.00`);
+
+    console.log("MOEDAS:");
+    console.log(`${Coin1.toFixed(0)} moeda(s) de R$ 1.00`);
+    console.log(`${cent50.toFixed(0)} moeda(s) de R$ 0.50`);
+    console.log(`${cent25.toFixed(0)} moeda(s) de R$ 0.25`);
+    console.log(`${cent10.toFixed(0)} moeda(s) de R$ 0.10`);
+    console.log(`${cent5.toFixed(0)} moeda(s) de R$ 0.05`);
+    console.log(`${cent1.toFixed(0)} moeda(s) de R$ 0.01`);
+
+}
+
+bankNotesWithCoins(576.73);
