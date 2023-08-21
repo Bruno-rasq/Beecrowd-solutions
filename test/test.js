4c3903520f85b71f7a0f80ccd486dc902687877e
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-21 17:19:41 -0300

    tsa rational. esta questão exige um pouco mais de matemática.. estou estudando uma resposta

--- test/test.js
+++ test/test.js
@@ -1,57 +1,39 @@
-function bankNotesWithCoins(n) {
+console.log(" 1022 - TDA rational");
 
-   
-    let CEM = Math.floor( n / 100 );
+//adiantando algumas partes
 
-    let CINQUENTA = Math.floor(( n - (100 * CEM) ) / 50);
+/*
+    inpult format 
 
-    let VINTE = Math.floor((n - (100 * CEM) - (50 * CINQUENTA)) / 20); 
+    number / number, operador( +, -, *, /) number / number
+    n1/d1 operador( +, -, *, /)  n2/d2
 
-    let DEZ = Math.floor((n - (100 * CEM) - ( 50 * CINQUENTA) - (20 * VINTE)) / 10);
+*/
 
-    let CINCO = Math.floor((n - (100 * CEM) - ( 50 * CINQUENTA) - (20 * VINTE) - (10 * DEZ)) / 5);
+response = null;
 
-    let DOIS = Math.floor((n - (100 * CEM) - ( 50 * CINQUENTA) - (20 * VINTE) - (10 * DEZ) - (5 * CINCO)) / 2);
+switch(operrador){
+    case '+':
+        response = (((n1 * d2) + (n2 * d1)) / (d1 * d2));
+        break;
 
+    case '-':
+        response = (((n1 * d2) - (n2 * d1)) / (d1 * d2));
+        break;
 
-    let restCEM = n % 100;
-    let restCINQ = restCEM % 50;
-    let restVINT = restCINQ % 20;
-    let restDEZ = restVINT % 10;
-    let restCINC = restDEZ % 5;
-    let restDOIS = restCINC % 2;
+    case '*':
+        response = ((n1 * n2) / (d1 * d2));
+        break;
 
-
-    let Coins = restDOIS * 100;
-
-    let Coin1 = Math.floor( Coins / 100);
-
-    let cent50 = Math.floor( (Coins - (100 * Coin1)) / 50);
-
-    let cent25 = Math.floor( (Coins - (100 * Coin1) - (50 * cent50)) / 25);
-
-    let cent10 = Math.floor( (Coins - (100 * Coin1) - (50 * cent50) - (25 * cent25)) / 10);
-
-    let cent5 = Math.floor( (Coins - (100 * Coin1) - (50 * cent50) - (25 * cent25) - (10 * cent10)) / 5);
-
-    let cent1 = Math.floor( (Coins - (100 * Coin1) - (50 * cent50) - (25 * cent25) - (10 * cent10) - (5 * cent5)) / 1);
-
-    console.log("NOTAS:");
-    console.log(`${CEM.toFixed(0)} nota(s) de R$ 100.00`);
-    console.log(`${CINQUENTA.toFixed(0)} nota(s) de R$ 50.00`);
-    console.log(`${VINTE.toFixed(0)} nota(s) de R$ 20.00`);
-    console.log(`${DEZ.toFixed(0)} nota(s) de R$ 10.00`);
-    console.log(`${CINCO.toFixed(0)} nota(s) de R$ 5.00`);
-    console.log(`${DOIS.toFixed(0)} nota(s) de R$ 2.00`);
-
-    console.log("MOEDAS:");
-    console.log(`${Coin1.toFixed(0)} moeda(s) de R$ 1.00`);
-    console.log(`${cent50.toFixed(0)} moeda(s) de R$ 0.50`);
-    console.log(`${cent25.toFixed(0)} moeda(s) de R$ 0.25`);
-    console.log(`${cent10.toFixed(0)} moeda(s) de R$ 0.10`);
-    console.log(`${cent5.toFixed(0)} moeda(s) de R$ 0.05`);
-    console.log(`${cent1.toFixed(0)} moeda(s) de R$ 0.01`);
+    case '/':
+        response = ((n1 * d2) / (n2 * d2));
 
 }
 
-bankNotesWithCoins(576.73);
+/*
+    resposta:
+
+    console.log(`${response} = ${simplify}`);
+    resposta da operação "=" simplificação da resposta ou caso não seja possivel a propria resposta
+
+ */
