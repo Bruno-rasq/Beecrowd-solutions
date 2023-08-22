5846e0803eca440cd18f2cd2a8979aec5b43e13c
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-22 18:39:48 -0300

    ok, cheguei na primeira resposta. preciso descobrir como simplifica-la

--- test/test.js
+++ test/test.js
@@ -11,32 +11,37 @@ let int = Number(lines.shift());
 
 // ----
 
-console.log(lines)
-
 for(let i = 0; i < int; i++){
     
    let currentData = lines[i].split(/\s+/).join('');
+
+   let n1 = Number(currentData[0]);
+   let d1 = Number(currentData[2]);
+   let n2 = Number(currentData[4]);
+   let d2 = Number(currentData[6]);
+
+   let response = "";
    
    switch(currentData[3]){
 
         case '+':
-            console.log("soma");
-            console.log(currentData);
+            response = ((n1 * d2) + (n2 * d1)) + "/" + ( d1 * d2);
+            console.log(response);
             break;
 
         case '-':
-            console.log("subtr");
-            console.log(currentData);
+            response = ((n1 * d2) - (n2 * d1)) + "/" + ( d1 * d2);
+            console.log(response);
             break;
 
         case '*':
-            console.log("multi");
-            console.log(currentData);
+            response = (n1 * n2) + "/" + ( d1 * d2);
+            console.log(response);
             break;
 
         case '/':
-            console.log("divis");
-            console.log(currentData);
+            response = (n1 * d2) + "/" + (n2 * d1);
+            console.log(response);
             break;
 
         default:
