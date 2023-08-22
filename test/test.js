3a881775aa34d69128e200ec3042ed35ed2db622
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-22 18:24:54 -0300

    ok. consegui dividir os dados, e usar um loop para interagir com o operador de cada dado

--- test/test.js
+++ test/test.js
@@ -10,3 +10,35 @@ let lines = input.split('\n');
 let int = Number(lines.shift());
 
 // ----
+
+console.log(lines)
+
+for(let i = 0; i < int; i++){
+    
+   let currentData = lines[i].split(/\s+/).join('');
+   
+   switch(currentData[3]){
+
+        case '+':
+            console.log("soma");
+            console.log(currentData);
+            break;
+
+        case '-':
+            console.log("subtr");
+            console.log(currentData);
+            break;
+
+        case '*':
+            console.log("multi");
+            console.log(currentData);
+            break;
+
+        case '/':
+            console.log("divis");
+            console.log(currentData);
+            break;
+
+        default:
+   };
+};
