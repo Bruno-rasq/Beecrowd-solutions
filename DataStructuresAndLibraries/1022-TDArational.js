81e1bffe6a443c3d10a5fb0f415369d1f5b64caa
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-22 17:01:57 -0300

    ok, entendi o  tipo da dado do input

--- DataStructuresAndLibraries/1022-TDArational.js
+++ DataStructuresAndLibraries/1022-TDArational.js
@@ -34,4 +34,21 @@ switch(operrador){
     console.log(`${response} = ${simplify}`);
     resposta da operação "=" simplificação da resposta ou caso não seja possivel a propria resposta
 
- */
+*/
+
+function test(str){
+
+    let split = str.split("+");
+
+    let arry1 = split[0].split("/");
+    let [ n1, d1 ] = arry1.map(item => parseFloat(item));
+
+    let arry2 = split[1].split("/");
+    let [ n2, d2 ] = arry2.map(item => parseFloat(item));
+
+    console.log(str)
+    console.log(n1, n2, d1, d2)
+    
+}
+
+test("1/2 + 3/4");
--- test/test.js
+++ test/test.js
@@ -1,18 +1,12 @@
-console.log(" 1022 - TDA rational");
+console.log(" 1022 - TDA rational ");
 
-function test(str){
+let input = `4
+1 / 2 + 3 / 4
+1 / 2 - 3 / 4
+2 / 3 * 6 / 6
+1 / 2 / 3 / 4`
 
-    let split = str.split("+");
+let lines = input.split('\n');
+let int = Number(lines.shift());
 
-    let arry1 = split[0].split("/");
-    let [ n1, d1 ] = arry1.map(item => parseFloat(item));
-
-    let arry2 = split[1].split("/");
-    let [ n2, d2 ] = arry2.map(item => parseFloat(item));
-
-    console.log(str)
-    console.log(n1, n2, d1, d2)
-    
-}
-
-test("1/2 + 3/4");
+// ----
