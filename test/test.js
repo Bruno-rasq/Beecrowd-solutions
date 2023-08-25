79997339bc7e765195a4c602fd3915f49250e5f7
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-25 20:00:34 -0300

    tratando os dados 1023 - drought

--- test/test.js
+++ test/test.js
@@ -1,91 +1,21 @@
-console.log(" 1022 - TDA rational ");
+console.log("1023 - drought");
 
-let input = `4
-1 / 2 + 3 / 4
-1 / 2 - 3 / 4
-2 / 3 * 6 / 6
-1 / 2 / 3 / 4`;
+const input = `3
+3 22
+2 11
+3 39`;
 
 let lines = input.split('\n');
-
 let int = Number(lines.shift());
 
+console.log(int);
 
-for (let i = 0; i < int; i++) {
+for(let i = 0; i < int; i++){
 
-    let currentData = lines[i].split(/\s+/);
-
-    let n1 = parseInt(currentData[0]);
-    let d1 = parseInt(currentData[2]);
-    let n2 = parseInt(currentData[4]);
-    let d2 = parseInt(currentData[6]);
-
-    let nom = null;
-    let den = null;
-    let simp = null;
-
-    switch (currentData[3]) {
-
-        case '+':
-            nom = ((n1 * d2) + (n2 * d1));
-            den = (d1 * d2);
-
-            simp = simplifyFrac(nom, den);
-
-            console.log(`${nom}/${den} = ${simp}`);
-            break;
-
-        case '-':
-            nom = ((n1 * d2) - (n2 * d1));
-            den = (d1 * d2);
-
-            simp = simplifyFrac(nom, den);
-
-            console.log(`${nom}/${den} = ${simp}`);
-            
-            break;
-
-        case '*':
-            nom = (n1 * n2);
-            den = (d1 * d2);
-
-            simp = simplifyFrac(nom, den);
-
-            console.log(`${nom}/${den} = ${simp}`);
-            break;
-
-        case '/':
-            nom = (n1 * d2);
-            den = (n2 * d1);
-
-            simp = simplifyFrac(nom, den);
-
-            console.log(`${nom}/${den} = ${simp}`);
-            break;
-
-        default:
-    };
-};
-
-function simplifyFrac(n, d){
-
-    let simplify = "";
-
-    for(let i = Math.max(n, d); i > 1; i--){
-        if ((n % i == 0) && (d % i == 0)) {
-            n /= i;
-            d /= i;
-        }
-    }
-
-    if(d == 1){
-        simplify = `${n}/1`;
-    } else {
-        simplify = `${n}/${d}`;
-    }
-
-    return simplify;
-};
+    let data = lines[i].split(/\s+/);
+    let [ moradores, consumo] = data;
 
 
+    console.log(data, Number(moradores), Number(consumo));
+}
 
