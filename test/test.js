75379b8f643479891d8ddc761914584bc602249a
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-25 20:36:52 -0300

    iniciando algumas partes do algoritmo

--- test/test.js
+++ test/test.js
@@ -10,12 +10,28 @@ let int = Number(lines.shift());
 
 console.log(int);
 
+
+
+// guarda as medias de consumo por residencia
+let mediasMoradores = null;
+
+// percorre todos as lines separandos os dados de moradores/consumo
 for(let i = 0; i < int; i++){
 
     let data = lines[i].split(/\s+/);
-    let [ moradores, consumo] = data;
+    let [ moradores, consumo ] = data;
 
+    // gera uma media de consumo para cada casa
+    let consumoPorMorador = (Number(consumo) / Number(moradores));
 
-    console.log(data, Number(moradores), Number(consumo));
+    mediasMoradores += consumoPorMorador;
+
 }
+
+// guarda a media de consumo por cidade
+let MediaCidade = Math.round(mediasMoradores / int);
+
+console.log(`consumo medio: ${MediaCidade.toFixed(2)} m3`);
+
+
 
