eaa024d099cc7ee1a9f2a201565900f86fb337ba
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-26 13:58:49 -0300

    algumas questões simplesmente não tem sentido. 2839 cocnluida

---
+++ Mathematics/2839-TheRangelStockings.js
@@ -0,0 +1,9 @@
+console.log("2839 - The Rangel stockings ")
+
+const input = '2';
+let lines = input.split('\n');
+
+let N = parseFloat(lines.shift());
+let response = N + 1;
+
+console.log(response);
