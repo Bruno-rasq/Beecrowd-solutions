062c6f0cdc4602dc5b9b074740efc732ee2ae87a
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-09-01 23:45:50 -0300

    pegando outra questão pra destrair a cabeça

--- test/test.js
+++ test/test.js
@@ -1,90 +1,36 @@
-console.log(" 1068 - parenthesis balance I ");
+console.log(" 2958 - The bad vibes walk");
 
-const input = `a+(b*c)-2-a 
-(a+b*(2-c)-2+a)*2 
-(a*b-(2+c) 
-2*(3-a))  
-)3+b*(2-c)(
-((((
-())(()`;
+const input = `3 3
+7V 1D 2D
+2D 1V 1V
+3V 5V 9D`
 
 let lines = input.split('\n');
+let [lin, col] = lines.shift().split(' ');
+lin = Number(lin);
+col = Number(col);
 
-for (let i = 0; i < lines.length; i++){
+for (let i = 0; i < lin; i++) {
 
-    let data = lines[i].trim().split('');
+    let data = lines[i].split(' ');
 
-    let parenthese = data.filter((char) => {
-        return char == "(" || char == ")"
-    });
+    console.log(data)
 
-    let haveParenthese = HaveParenthesis(parenthese);
+    // let life = data.filter((quest) => {
+    //     return quest.includes('V')
+    // });
 
-    if ( haveParenthese == false ){
-        break;
+    // let college = data.filter((quest) => {
+    //     return quest.includes('D')
+    // });
 
-    } else if( haveParenthese == true ){
+    // let V = [];
+    // let D = [];
 
-        console.log(parenthese)
+    // V.push(life);
+    // D.push(college);
+    
+    // console.log(V)
+    // console.log(D)
 
-        ValidByCount(parenthese);
-        ValidFormat(parenthese);
-
-        console.log(" ")
-
-    }
-
-};
-
-
-
-function HaveParenthesis(parenthese){
-    if(parenthese.length == null){
-        return false
-    } else {
-        return true
-    };
-};
-
-
-function ValidByCount(arry){
-
-    let count = 0;
-
-    for (let i = 0; i < arry.length; i++){
-
-        if(arry[i] == "(" ){
-            count++
-        } else if ( arry[i] == ")" ){
-            count--
-        }
-    };
-
-    if( count == 0 ){
-        return console.log('correct count');
-    } else {
-        return console.log('incorrect count');
-    };
-
-};
-
-function ValidFormat ( arry ){
-
-    let close = arry.filter(char => {
-        return char == ")"
-    });
-
-    let open = arry.filter(char => {
-        return char == "("
-    });
-
-    if ( arry.length % 2 == 0 && arry[0] == "(" && close.length == open.length ){
-        return console.log('correct format')
-
-    } else if ( arry[0] == ")" || arry.length % 2 != 0 || close.length !== open.length){
-        return console.log('incorrect format')
-    };
-
-};
-
-
+}
