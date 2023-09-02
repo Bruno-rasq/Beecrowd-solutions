c533bb66a508e971349ce5cae07eb4a00c6972e5
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-09-01 22:33:33 -0300

    essa questão possue uma infinidade de casos de teste que torna ela muito dificil

--- Tried/1068-parenthesisBalanceI.js
+++ Tried/1068-parenthesisBalanceI.js
@@ -10,25 +10,48 @@ let lines = input.split('\n');
 
 for (let i = 0; i < lines.length; i++){
 
-    let data = lines[i].split('');
+    let data = lines[i].trim().split('');
 
-    let open = data.filter((char) => {
-        return char == '('
+    let parenthese = data.filter((char) => {
+        return char == "(" || char == ")"
     });
 
-    let close = data.filter((char) => {
-        return char == ')'
+    let close = parenthese.filter(char => {
+        return char == ")"
     });
 
-    if(open.length == null && close.length == null){
+    let open = parenthese.filter(char => {
+        return char == "("
+    });
+
+    if ( parenthese.length == null ){
         break;
 
-    } else if(open.length == close.length && open.length !== null && close.length !== null){
-        console.log('correct')
+    } else if ( parenthese.length % 2 == 0 && parenthese[0] == "("  && open.length == close.length ){
+        console.log('correct');
 
-    } else if(open.length !== close.length) {
-        console.log('incorrect')
+    } else if ( parenthese[0] == ")" && parenthese.length % 2 == 0 ){
+        console.log("incorrect");
 
+    } else if ( parenthese.length % 2 !== 0 || open.length !== close.lenght){
+        console.log('incorrect');
     };
 
 };
+
+
+/*
+    PROBLEMA:
+        - código está fazendo o que devia porém com um erro
+
+        - se a quantidade de abertura de parenteses for igual a de fechamento
+        basicamente quer dizer que está certo mas tecnicamente não assegura que a formatação estaja tbm.
+
+        (  ( )  -> incorreto
+
+        ")" "( )" "(" -> nesse caso a qnt de abertura e fechamento é igual porem a formatação está incorreta
+
+    NOTA:
+        - concertei o problema da formatação de parenteses e mesmo assim não passou no teste
+
+*/
--- test/test.js
+++ test/test.js
@@ -4,31 +4,36 @@ const input = `a+(b*c)-2-a
 (a+b*(2-c)-2+a)*2 
 (a*b-(2+c) 
 2*(3-a))  
-)3+b*(2-c)(`;
+)3+b*(2-c)(
+((((`;
 
 let lines = input.split('\n');
 
 for (let i = 0; i < lines.length; i++){
 
-    let data = lines[i].split('');
+    let data = lines[i].trim().split('');
 
-    let open = data.filter((char) => {
-        return char == '('
+    let parenthese = data.filter((char) => {
+        return char == "(" || char == ")"
     });
 
-    let close = data.filter((char) => {
-        return char == ')'
-    });
+    console.log(parenthese)
 
-    if(open.length == null && close.length == null){
-        break;
+    let count = 0;
 
-    } else if(open.length == close.length && open.length !== null && close.length !== null){
-        console.log('correct')
+    for ( let n = 0; n < parenthese.length; n++){
 
-    } else if(open.length !== close.length) {
-        console.log('incorrect')
-
+        if(parenthese[n] == "("){
+            count++
+        } else if ( parenthese[n] == ")"){
+            count--
+        }
     };
 
+    console.log(count)
+
+    
+
 };
+
+
