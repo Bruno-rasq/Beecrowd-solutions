79036a0e99a8a683ea7443262f24fa1c9240bab1
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-09-13 12:49:22 -0300

    1037 concluido

---
+++ beginner/1037-Interval.js
@@ -0,0 +1,25 @@
+console.log(" 1037 - Interval ");
+
+let input = `25.01`;
+let lines = input.split('\n');
+
+let Num = Number(lines[0]);
+
+if(Num < 0 ){
+    console.log("Fora de intervalo")
+
+} else if ( Num >= 0 && Num <= 25 ){
+    console.log("Intervalo [0,25]")
+
+} else if ( Num >= 25 && Num <= 50 ){
+    console.log("Intervalo (25,50]")
+
+} else if( Num >= 50 && Num <= 75 ){
+    console.log("Intervalo (50,75]")
+
+} else if ( Num >= 75 && Num <= 100 ){
+    console.log("Intervalo (75,100]")
+
+} else if ( Num > 100 ){
+    console.log("Fora de intervalo")
+};
--- test/test.js
+++ test/test.js
@@ -1,23 +1,4 @@
-console.log(" 1036 - Bhaskara's Formula ");
+console.log(" 1037 - Interval ");
 
-let input = `10.0 3.0 5.0`;
-let lines = input.split(' ');
-
-let A = parseFloat(lines[0])
-let B = parseFloat(lines[1])
-let C = parseFloat(lines[2])
-
-//x = (-b ± √(b² – 4ac)) / (2a) 
-let delta = (B*B) - (4*A*C);
-
-if (delta >= 0 && A !== 0){
-
-    let r1 = (-B + Math.sqrt(delta)) / (2*A);
-    let r2 = (-B - Math.sqrt(delta)) / (2*A);
-
-    console.log(`R1 = ${r1.toFixed(5)}`)
-    console.log(`R2 = ${r2.toFixed(5)}`)
-
-} else {
-    console.log("Impossivel calcular");
-};
+let input = ``;
+let lines = input.split('\n');
