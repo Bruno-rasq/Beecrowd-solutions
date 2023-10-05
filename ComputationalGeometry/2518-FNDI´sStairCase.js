ea96b25f8cd8f3a97d78396dcf9f247e496433c6
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-10-05 00:48:03 -0300

    2518 concluido
    minha resposta esta correta porem peguei a resposta de outro dev para complementar a minha

--- ComputationalGeometry/2518-FNDI´sStairCase.js
+++ ComputationalGeometry/2518-FNDI´sStairCase.js
@@ -1,12 +1,26 @@
-// let input = require('fs').readFileSync("./test/stdin", 'utf8');
-// let lines = input.split('\n');
-
 console.log(" 2518 - FNDI's Staircase ");
 
 const input = `4
 3 4 30`;
 
-let lines = input.split('\n').map((line) => line.split(" ", 3).map((value) => Number.parseInt(value, 10)));
+/*
+
+[MINHA RESPOSTA];
+
+    let lines = input.split('\n');
+    let d = Number(lines.shift());
+
+    let data = lines[0].split(' ').map(item => parseInt(item));
+    let [ h, c, l ] = data
+
+    let areaRampCalc = (Math.hypot(h, c) * d * l) * 1e-4;
+
+    console.log(areaRampCalc.toFixed(4));
+
+*/
+
+let lines = input.split('\n').map((line) => line.split(" ", 3)
+.map((value) => Number.parseInt(value, 10)));
 
 let calcRampArea = (d, h, c, l) => (Math.hypot(h, c) * d * l) * 1e-4;
 
