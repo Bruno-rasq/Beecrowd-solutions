a8794f8ef189eff7f2ddff60079b45f7c42cc3bc
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-10-10 19:31:17 -0300

    1088 tried

--- test/test.js
+++ test/test.js
@@ -1,12 +1,14 @@
 // let input = require('fs').readFileSync("./test/stdin", 'utf8');
 // let lines = input.split('\n');
 
-console.log(" 1110 - Throwing Cards Away ");
+console.log(" 1088 - Bubbles and Buckets ");
 
-const input = `7
-19
-10
-6
+const input = `5 1 5 3 4 2
+5 5 1 3 4 2
+5 1 2 3 4 5
+6 3 5 2 1 4 6
+5 5 4 3 2 1
+6 6 5 4 3 2 1
 0`;
 
 let lines = input.split('\n');
