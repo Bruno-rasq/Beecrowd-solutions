1194644a9e7930a4732310d3bd98305857e7f54d
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-11-05 02:47:01 -0200

    parando...

--- test/test.js
+++ test/test.js
@@ -1,9 +1,12 @@
 // let input = require('fs').readFileSync("./test/stdin", 'utf8');
 // let lines = input.split('\n');
 
-console.log(" 1071 - Sum of Consecutive Odd Numbers I ");
+console.log(" 1273 - Justifier ");
 
-const input = `12
-12`;
+const input = `3
+3 xYz Yxz
+3 abc cba
+6 ABCDEF CBAEDF`;
 
-let lines = input.split('\n')
+let lines = input.split('\n').map(el => Number(el));
+let int = Number(lines.shift());
