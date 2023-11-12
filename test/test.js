e3a3b08de7a5e43934a6ad973b12dcf4b53e463f
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-11-12 19:31:11 -0200

    1768 parando...

--- test/test.js
+++ test/test.js
@@ -1,10 +1,38 @@
 // let input = require('fs').readFileSync("./test/stdin", 'utf8');
 // let lines = input.split('\n');
 
-console.log(" 1030 - Flavious Josephus Legend ");
+console.log(" 1768 -  ");
 
-const input = `2
-5 2
-6 3`;
+const input = `9
+5`;
 
 let lines = input.split('\n');
+
+for(let i = 0; i < lines.length; i++){
+
+    let widthTree = Number(lines[i])
+    let heightTree = Math.round(widthTree/2)
+
+    //Insert your code here.
+    
+}
+
+function TreeRoot(width){
+
+    let root = ['*', '*', '*'];
+    let branch = ['*'];
+
+    while(root.length < width){
+        root.unshift(' ')
+        root.push(' ')
+    }
+
+    while(branch.length < width){
+        branch.unshift(' ')
+        branch.push(' ')
+    }
+
+    let arr = [branch.join(' '), root.join(' ')]
+
+    return arr.join('\n')
+}
